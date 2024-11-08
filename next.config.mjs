c4d79db9883e8e5e40fe0f5d0bb2882deb9d9a57
Gitit2meBaby <thisworldofdans@gmail.com>
2024-11-08 11:10:24 +0700

    build file changes

--- next.config.mjs
+++ next.config.mjs
@@ -7,11 +7,21 @@ const nextConfig = {
       {
         protocol: "https",
         hostname: "gentle-pond-0aa028e00.5.azurestaticapps.net",
+        pathname: "/**",
       },
     ],
     formats: ["image/webp"],
   },
-  assetPrefix: "https://gentle-pond-0aa028e00.5.azurestaticapps.net",
+  experimental: {
+    outputFileTracingRoot: "../../", // Add this for monorepo
+    outputFileTracingExcludes: {
+      "*": [
+        "node_modules/@swc/core-linux-x64-gnu",
+        "node_modules/@swc/core-linux-x64-musl",
+        "node_modules/@esbuild/linux-x64",
+      ],
+    },
+  },
 };
 
 export default nextConfig;
